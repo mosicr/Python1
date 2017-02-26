c5ba09ae9d11543228a475baf5e98029289edf70
mosicr <ranko.mosic@bankofamerica.com>
2017-02-26 08:46:39 -0500

    Update testdf.py

--- testdf.py
+++ testdf.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+#display dataframe records when ran from command line via python file_name.py
 import pandas as pd
 pd.set_option('display.max_rows', 999)
 d = {'one' : [1., 2., 3., 4.],
